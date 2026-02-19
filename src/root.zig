0641f5f459b920420e4209fb98af2c873e4e1fa7
arthurcornil <102752044+arthurcornil@users.noreply.github.com>
2026-02-19 09:12:47 +0100

    expose Terminal struct

--- src/root.zig
+++ src/root.zig
@@ -2,8 +2,10 @@ pub const layout = @import("layout.zig");
 pub const pane = @import("pane.zig");
 pub const primitive = @import("primitive.zig");
 pub const Ansi = @import("ansi.zig");
+pub const terminal = @import("terminal.zig");
 
 pub const Layout = layout.Layout;
 pub const Col = layout.Col;
 pub const Pane = pane.Pane;
 pub const Content = pane.Content;
+pub const Terminal = terminal.Terminal;
